ac2251830aaf5d5fbb263529f670d9a5e55ccc02
wsf <2630002413@qq.com>
2018-09-04 18:11:18 +0800

    高德地图项目上传

---
+++ app/src/main/java/com/moyu/gaodemapproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.moyu.gaodemapproject;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // StatusBarUtils.initStatusBar(this,R.color.transparent);
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
+    }
+}
